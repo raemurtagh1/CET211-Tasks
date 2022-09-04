21a44b7ebb3022cc3ce9f372263a32455fadfbf8
Rachael Murtagh <bh55gi@student.sunderland.ac.uk>
2022-09-04 20:48:08 +0100

    Highest-Integer-Swap

---
+++ Program3.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace HighestIntegerSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iFirst;
+            int iSecond;
+
+            Console.Write("Please enter a number between 1 and 10 - ");
+            iFirst = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            while (iFirst == 0 || iFirst > 10)
+            {
+                Console.WriteLine("The number you have entered is invalid.");
+                Console.Write("Please enter another number between 1 and 10 - ");
+                iFirst = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+            }
+
+            //Clearing Errors
+            Console.Clear();
+            Console.WriteLine("Please enter a number between 1 and 10 - " + iFirst);
+            //End of Clearing Errors
+
+            Console.Write("Please enter another number between 1 and 10 - ");
+            iSecond = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine();
+
+            while (iSecond == 0 || iSecond > 10)
+            {
+                Console.WriteLine("The number you have entered is invalid.");
+                Console.Write("Please enter another number between 1 and 10 - ");
+                iSecond = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("The first number you entered was - " + iFirst);
+            Console.WriteLine("The second number you entered was - " + iSecond);
+            Console.WriteLine();
+
+            if (iSecond > iFirst)
+            {
+                Console.WriteLine(" " + iSecond);
+                Console.WriteLine(" " + iFirst);
+
+                Console.WriteLine();
+                Console.WriteLine(" The numbers have been swapped.");
+            }
+            else if (iFirst == iSecond)
+            {
+                Console.WriteLine(" " + iFirst);
+                Console.WriteLine(" " + iSecond);
+
+                Console.WriteLine();
+                Console.WriteLine(" The numbers are equal.");
+            }
+            else
+            {
+                Console.WriteLine(" " + iFirst);
+                Console.WriteLine(" " + iSecond);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();
+        }
+    }
+}
